69766deba92ca81196258651356cf530ac43d185
developerw707 <127234474+developerw707@users.noreply.github.com>
2023-05-08 17:06:25 -0700

    Update collection.ts
    Updated collection page by dev7

--- src/fragments/sanity/pages/collection.ts
+++ src/fragments/sanity/pages/collection.ts
@@ -5,20 +5,45 @@ import {MODULES} from '../modules';
 import {SEO_SHOPIFY} from '../seoShopify';
 
 export const COLLECTION_PAGE = groq`
-  _id,
-  colorTheme->{
-    ${COLOR_THEME}
-  },
-  (showHero == true) => {
-    hero {
-      ${HERO_COLLECTION}
-    },
-  },
-  modules[] {
-    ${MODULES}
-  },
-  ${SEO_SHOPIFY},
-  "slug": store.slug.current,
-  "sortOrder": store.sortOrder,
-  "title": store.title,
-`;
+ <li class="contact-item">
+
+            <div class="icon-box">
+              <ion-icon name="phone-portrait-outline"></ion-icon>
+            </div>
+
+            <div class="contact-info">
+              <p class="contact-title">Phone</p>
+
+              <a href="tel:+12133522795" class="contact-link">+1 (213) 352-2795</a>
+            </div>
+
+          </li>
+
+          <li class="contact-item">
+
+            <div class="icon-box">
+              <ion-icon name="calendar-outline"></ion-icon>
+            </div>
+
+            <div class="contact-info">
+              <p class="contact-title">Birthday</p>
+
+              <time datetime="1982-06-23">June 23, 1982</time>
+            </div>
+
+          </li>
+
+          <li class="contact-item">
+
+            <div class="icon-box">
+              <ion-icon name="location-outline"></ion-icon>
+            </div>
+
+            <div class="contact-info">
+              <p class="contact-title">Location</p>
+
+              <address>Sacramento, California, USA</address>
+            </div>
+
+          </li>
+
